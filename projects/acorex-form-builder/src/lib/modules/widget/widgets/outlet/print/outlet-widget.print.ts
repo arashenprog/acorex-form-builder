fff3571198d3ec2e7d84951d1344d3bdcc86e264
Elahe Lolaki <e.lolaki@istd.com>
2020-01-22 19:30:54 +0330

    table print

--- projects/acorex-form-builder/src/lib/modules/widget/widgets/outlet/print/outlet-widget.print.ts
+++ projects/acorex-form-builder/src/lib/modules/widget/widgets/outlet/print/outlet-widget.print.ts
@@ -1,12 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { AXFTemplateService } from '../../../services/template/template.service';
+import { AXFWidgetPrint } from '../../../config/widget';
 
 @Component({
     selector: '[axf-outlet]',
     templateUrl: './outlet-widget.print.html'
 })
-export class AXFOutletWidgetPrint implements OnInit {
-    constructor() { }
+export class AXFOutletWidgetPrint extends AXFWidgetPrint implements OnInit {
+    widgetId: string;
+    
+    constructor( private templateService: AXFTemplateService) { 
+        super();
+    }
+   
 
-    ngOnInit(): void { }
+    ngOnInit() {
+        this.templateService.get(this.widgetId).then(c => {
+            this.widgets = this.widgetService.parse(c.template).options.widgets;
+            this.refresh();
+        });
+    }
 }
 
