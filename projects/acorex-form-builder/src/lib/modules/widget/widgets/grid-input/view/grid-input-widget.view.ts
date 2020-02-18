3fc0e108a540d3eb0841887d9b2726bd0d8a40c7
Elahe Lolaki <e.lolaki@istd.com>
2020-02-18 10:23:26 +0330

    grid changes

--- projects/acorex-form-builder/src/lib/modules/widget/widgets/grid-input/view/grid-input-widget.view.ts
+++ projects/acorex-form-builder/src/lib/modules/widget/widgets/grid-input/view/grid-input-widget.view.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, ElementRef, ViewChild, ChangeDetectorRef, ChangeDetectionStrategy } from '@angular/core';
-import { AXFWidgetView } from '../../../config/widget'; 
+import { AXFWidgetView } from '../../../config/widget';
 import { AXFDataSourceOption } from '../../../../property-editor/editors/data-source/data-source.class';
+import { AXPopupService } from 'acorex-ui';
+import { SignaturePadPage } from '../../signature-input/signaturepad.page';
 
 @Component({
     templateUrl: './grid-input-widget.view.html',
@@ -11,17 +13,18 @@ export class AXFGridInputWidgetView extends AXFWidgetView {
 
     @ViewChild("el") el: ElementRef<HTMLElement>;
 
-    value: any[]=[];
+    value: any[] = [];
     dataSource: AXFDataSourceOption;
     allowAddDelete: boolean;
 
-    constructor(private cdr: ChangeDetectorRef) {
+    constructor(private cdr: ChangeDetectorRef, private popupService: AXPopupService) {
         super()
     }
 
     onRender(): void {
-        if(this.dataSource.mode=="remote")
-        {
+        if (this.value && this.value.length > 0)
+            return;
+        if (this.dataSource.mode == "remote") {
             this.dataSource.dataSource.params.forEach(p => {
                 if (typeof (p.value) === 'string' && p.value.startsWith('$')) {
                     p.value = this.resolveProperty(p.value);
@@ -29,26 +32,61 @@ export class AXFGridInputWidgetView extends AXFWidgetView {
             });
             this.dataService.getList(this.dataSource.dataSource.name, this.dataSource.dataSource.params).then(items => {
                 if (items && items.length) {
-                    this.value= items; 
+                    this.value = items;
                     this.cdr.markForCheck();
-                }  
-            }); 
+                }
+            });
         }
-        else
-        {
-            this.value=this.dataSource.dataItems;
+        else {
+            this.value = this.dataSource.dataItems;
             this.cdr.markForCheck();
         }
-        
-    }
-
-    addRowClick()
-    {
 
     }
 
-    deleteClick(i)
-    {
-        
+    addRowClick() {
+        let param: any = { id: new Date().getTime() };
+        this.dataSource.columns.forEach(f => {
+            if (!f.fillByUser) {
+                switch (f.type) {
+                    case "string":
+                        param[f.fieldName] = "";
+                        break;
+                    case "number":
+                        param[f.fieldName] = 0;
+                        break;
+                    case "boolean":
+                        param[f.fieldName] = false;
+                        break;
+                    case "date":
+                        param[f.fieldName] = Date();
+                        break;
+                    case "time":
+                        param[f.fieldName] = "00:00";
+                        break;                      
+                    default:
+                        break;
+                }
+
+            }
+        })
+        this.value.push(param);
+    }
+
+    deleteClick(i) {
+        this.value.splice(i, 1);
+    }
+
+    setSignatureClick() {
+        this.popupService.open(SignaturePadPage, {
+            title: 'Signature',
+            size: 'md',
+            data: {
+                data: this.value
+            }
+        }).closed(c => {
+            this.value = c.data;
+            this.cdr.markForCheck();
+        });
     }
 }
