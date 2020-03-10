21f06d3ada6fe01ae34aac5bb205d001150af9c9
arashenprog <arash.oshnoudi@gmail.com>
2020-03-10 18:01:56 +0330

    add tag

--- projects/acorex-form-builder/src/lib/modules/widget/widgets/repeater/repeater.module.ts
+++ projects/acorex-form-builder/src/lib/modules/widget/widgets/repeater/repeater.module.ts
@@ -7,7 +7,7 @@ import { AXFWidgetService } from '../../services/widget.service';
 import { ACoreXUIModule } from 'acorex-ui';
 import { AXFWidgetSharedModule } from '../../shared/shared.module';
 import { AXFTextEditorComponent } from '../../../property-editor/editors/text/text.editor';
-import { AXF_STYLE_GENERAL_PROPERTIES, AXF_BG_COLOR_PROPERTY, AXF_BOX_STYLE_PROPERTY, AXF_HORIZONTAL_ALIGNMENT_PROPERTY, AXF_VERTICAL_ALIGNMENT_PROPERTY, AXF_DS_LIST_PROPERTY, AXF_NAME_PROPERTY, AXF_VALUE_CHANGE_EVENT } from '../../config/general-properties';
+import { AXF_STYLE_GENERAL_PROPERTIES, AXF_BG_COLOR_PROPERTY, AXF_BOX_STYLE_PROPERTY, AXF_HORIZONTAL_ALIGNMENT_PROPERTY, AXF_VERTICAL_ALIGNMENT_PROPERTY, AXF_DS_LIST_PROPERTY, AXF_NAME_PROPERTY, AXF_VALUE_CHANGE_EVENT, AXF_TAG_PROPERTY } from '../../config/general-properties';
 import { AXFBoxStyleBoxSizeValue } from '../../../property-editor/editors/style/box-style/box-style.class';
 
 export const COMPONENTS = [AXFRepeaterWidgetDesigner, AXFRepeaterWidgetView, AXFRepeaterWidgetPrint]
@@ -22,44 +22,45 @@ export const COMPONENTS = [AXFRepeaterWidgetDesigner, AXFRepeaterWidgetView, AXF
 export class AXFRepeaterlWidgetModule {
     constructor(service: AXFWidgetService) {
         service.register({
-            title: "Repeater",
-            hint: "Repeater container element",
-            icon: "fas fa-repeat", 
-            category: "Layout",
+            title: 'Repeater',
+            hint: 'Repeater container element',
+            icon: 'fas fa-repeat',
+            category: 'Layout',
             visible: true,
-            name: "repeater",
+            name: 'repeater',
             designerClass: AXFRepeaterWidgetDesigner,
             printClass: AXFRepeaterWidgetPrint,
             container: true,
-            draggable:false,
+            draggable: false,
             viewClass: AXFRepeaterWidgetView,
             options: {
                 boxStyle: {
-                    border: new AXFBoxStyleBoxSizeValue("1"),
-                    padding: new AXFBoxStyleBoxSizeValue("0"),
-                    margin: new AXFBoxStyleBoxSizeValue("5", "0", "5", "0")
+                    border: new AXFBoxStyleBoxSizeValue('1'),
+                    padding: new AXFBoxStyleBoxSizeValue('0'),
+                    margin: new AXFBoxStyleBoxSizeValue('5', '0', '5', '0')
                 },
-                bgColor: "inherit",
+                bgColor: 'inherit',
                 dataSource: {
-                    mode: "manual",
+                    mode: 'manual',
                     dataSource: {},
                     columns: [],
-                    dataItems: [] 
+                    dataItems: []
                 },
             },
             properties: [
                 {
-                    name: "showHeader",
-                    category: "General",
-                    defaultValue:false,
-                    title: "Show Header",
-                    editor: "CheckboxEditor",
-                    options: { label:"Show Header"}
+                    name: 'showHeader',
+                    category: 'General',
+                    defaultValue: false,
+                    title: 'Show Header',
+                    editor: 'CheckboxEditor',
+                    options: { label: 'Show Header' }
                 },
                 AXF_BG_COLOR_PROPERTY,
                 AXF_DS_LIST_PROPERTY,
-                AXF_BOX_STYLE_PROPERTY, 
-                AXF_NAME_PROPERTY, 
+                AXF_BOX_STYLE_PROPERTY,
+                AXF_NAME_PROPERTY,
+                AXF_TAG_PROPERTY,
                 AXF_VALUE_CHANGE_EVENT,
                 {
                     name: 'allowAdd',
@@ -69,7 +70,7 @@ export class AXFRepeaterlWidgetModule {
                     editor: 'CheckboxEditor',
                     options: { label: 'Allow Add Item' },
                     visible: (options: any) => {
-                        if(options.dataSource && options.dataSource.mode == "manual")
+                        if (options.dataSource && options.dataSource.mode == 'manual')
                             return true;
                         else
                             return false;
