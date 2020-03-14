b08d48f45753372570a50835f6bf41d38e486f1a
Elahe Lolaki <e.lolaki@istd.com>
2020-03-14 17:22:02 +0330

    list input css

--- projects/acorex-form-builder/src/lib/modules/widget/widgets/list-input/print/list-input-widget.print.ts
+++ projects/acorex-form-builder/src/lib/modules/widget/widgets/list-input/print/list-input-widget.print.ts
@@ -5,7 +5,7 @@ import { AXFDataService } from '../../../services/data.service';
 
 @Component({
     templateUrl: './list-input-widget.print.html',
-    changeDetection:ChangeDetectionStrategy.OnPush
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AXFListInputWidgetPrint extends AXFWidgetPrint {
     @ViewChild("el") el: ElementRef<HTMLElement>;
@@ -14,10 +14,11 @@ export class AXFListInputWidgetPrint extends AXFWidgetPrint {
     mode: string;
     direction: string;
     alignCheck: string;
+    alignCheckNew: string;
     viewType: string;
     visible: boolean = true;
-    printMode:string;
-    columns:number;
+    printMode: string;
+    columns: number;
     constructor(private cdr: ChangeDetectorRef) {
         super()
     }
@@ -25,14 +26,13 @@ export class AXFListInputWidgetPrint extends AXFWidgetPrint {
     onRender(): void {
         if (this.el)
             this.applyStyle(this.el.nativeElement);
-        if (this.value==undefined && this.value==null && this.dataSource.mode =='manual')
-        {
-            this.value=[]; 
+        if (this.value == undefined && this.value == null && this.dataSource.mode == 'manual') {
+            this.value = [];
             {
                 this.value.push(this.dataSource.dataItems[0][this.dataSource.columns[0].fieldName]);
                 this.cdr.markForCheck();
             }
-        }    
+        }
     }
 
     ngAfterViewInit() {
@@ -40,30 +40,125 @@ export class AXFListInputWidgetPrint extends AXFWidgetPrint {
     }
 
     refresh() {
-        
+
         if (this.dataSource.mode == "remote") {
             this.dataService.getList(
                 this.dataSource.dataSource.name,
                 this.dataSource.dataSource.params
             ).then(c => {
                 this.dataSource.dataItems = c;
-                if (this.value==undefined && this.value==null)
-                {
-                    this.value=[]; 
+                if (this.value == undefined && this.value == null) {
+                    this.value = [];
                     {
                         this.value.push(this.dataSource.dataItems[0][this.dataSource.columns[0].fieldName]);
                         this.cdr.markForCheck();
                     }
-                }  
+                }
                 this.cdr.markForCheck();
             })
         }
     }
 
-    getStyles(mode) { 
-        const styles = {
-            'border-radius': mode=='single' ? 100 + "%" : 0
-        };
+    getStylesRadio(viewType, alignCheckNew, alignCheck) {
+        let styles: any = {};
+        if (viewType == 'both') {
+            switch (alignCheckNew) {
+                case 'ItopCrightSleft':
+                    styles = {
+                        'order': '2'
+                    };
+                    break;
+                case 'ItopCleftSright':
+                    styles = {
+                        'order': '1'
+                    };
+                    break;
+                default:
+                    break;
+            }
+        }
+        else {
+            switch (alignCheck) {
+                case 'left':
+                    styles = { 'order': '1' }
+                    break;
+                case 'top':
+                    styles = { 'order': '1', 'flex': '1 0 100%' }
+                    break;
+                    case 'right':
+                        styles = { 'right': '2px' } 
+                        break;    
+                default:
+                    break;
+            }
+        }
         return styles;
     }
+
+    getStylesSpan(viewType, alignCheckNew, alignCheck) {
+        let styles: any = {};
+        if (viewType == 'both') {
+            switch (alignCheckNew) {
+                case 'ItopCrightSleft':
+                    styles = {
+                        'order': '1'
+                    };
+                    break;
+                case 'ItopCleftSright':
+                    styles = {
+                        'order': '2'
+                    };
+                    break;
+                default:
+                    break;
+            }
+        }
+        else {
+            switch (alignCheck) {
+                case 'left':
+                    styles = { 'order': '2' }
+                    break;
+                case 'top':
+                    styles = { 'order': '2', 'margin-top': '22px' }
+                    break;
+                default:
+                    break;
+            }
+        }
+        return styles;
+    }
+
+    getStylesImage(viewType, alignCheckNew, alignCheck) {
+        let styles: any = {};
+        if (viewType == 'both') {
+            switch (alignCheckNew) {
+                case 'ItopCrightSleft':
+                    styles = {
+                        'order': '-1', 'flex': '1 0 100%'
+                    };
+                    break;
+                case 'ItopCleftSright':
+                    styles = {
+                        'order': '-1', 'flex': '1 0 100%'
+                    };
+                    break;
+                default:
+                    break;
+            }
+        }
+        else {
+            switch (alignCheck) {
+                case 'left':
+                    styles = { 'order': '2' }
+                    break;
+                case 'top':
+                    styles = { 'order': '2', 'margin-top': '22px' }
+                    break;
+                default:
+                    break;
+            }
+        }
+        return styles;
+    }
+
 }
