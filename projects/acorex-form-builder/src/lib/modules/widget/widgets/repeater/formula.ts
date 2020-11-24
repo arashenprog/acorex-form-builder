ce26590b4a7807aa2e789e14760cf0f77f9a8ae7
Elahe Lolaki <e.lolaki@istd.com>
2020-11-24 14:59:52 +0330

    add new formula

--- projects/acorex-form-builder/src/lib/modules/widget/widgets/repeater/formula.ts
+++ projects/acorex-form-builder/src/lib/modules/widget/widgets/repeater/formula.ts
@@ -16,35 +16,43 @@ export class AXFRepeaterlWidgetFormula {
         let i = 0;
         (this.ww as any).allItems().forEach(element => {
             const w = service.getWidget(`${this.ww.getName()}[${i}].${widget}`);
-            if(w)
-            {
+            if (w) {
                 if (!condition || w.value == condition) {
                     items.push(w.dataContext[textField]);
                 }
             }
-            else
-            {
-                if(element[widget]== condition)
-                items.push(element[textField]);
-            } 
+            else {
+                if (element[widget] == condition)
+                    items.push(element[textField]);
+            }
             i++;
         });
 
         return items.join(',');//.map(c => c.dataContext[textField])
     }
 
-    sum(widget: string):number
-    {
+    sum(widget: string): number {
         debugger
         const service = (this.ww as any).dataService as AXFDataService;
         const items: any[] = [];
         let i = 0;
-        let sum=0;
+        let sum = 0;
         (this.ww as any).bodyRows.forEach(element => {
             const w = service.getWidget(`${this.ww.getName()}[${i}].${widget}`);
-            sum += (w.value==undefined?0:parseFloat(w.value))
+            sum += (w.value == undefined ? 0 : parseFloat(w.value))
             i++;
         });
-return sum;
+        return sum;
+    }
+
+    addNewRow(obj)
+    {
+        debugger
+        let view= (this.ww as any);
+        if (view.rowTemplate) {
+            let cloned=view.widgetService.clone(view.rowTemplate);
+            cloned.dataContext = obj;
+            view.bodyRows.push(cloned);
+        }
     }
 }
