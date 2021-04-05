9c9720ec29b6def7cdae1e251d92537bf7935671
Elahe Lolaki <e.lolaki@istd.com>
2021-04-05 15:04:38 +0430

    image save after zoom

--- projects/acorex-form-builder/src/lib/modules/widget/widgets/image-input/imagemodal.page.ts
+++ projects/acorex-form-builder/src/lib/modules/widget/widgets/image-input/imagemodal.page.ts
@@ -11,15 +11,16 @@ export class ImageModalPage extends AXBasePageComponent {
 
     @ViewChild("el") el: ElementRef<HTMLElement>;
     constructor(private resolveService: AXFUrlResolverService) {
-        super(); 
+        super();
     }
 
     @Input()
     public value: string;
 
-    images: string[] = []; 
-    changeVal:any;
-    degree:number=0; 
+    images: string[] = [];
+    changeVal: any;
+    degree: number = 0;
+    scale: number = 1;
     config: any = {
         btnContainerClass: '',
         btnClass: 'default',
@@ -62,26 +63,33 @@ export class ImageModalPage extends AXBasePageComponent {
         this.images = [this.value];
 
         setTimeout(() => {
-            let that= this;
-            document.querySelectorAll(".btn-container .default")        
-            .forEach(f=>f.addEventListener('click',async function(event) {                    
-                    if((event.target as any).className=="fa-undo fas" || (event.target as any).lastChild.className=="fa-undo fas")
-                    { 
-                        that.degree+=90;
-                        if(that.degree==360) that.degree=0; 
+            let that = this;
+            document.querySelectorAll(".btn-container .default")
+                .forEach(f => f.addEventListener('click', async function (event) {
+                    if ((event.target as any).className == "fa-undo fas" || ((event.target as any).lastChild != null && (event.target as any).lastChild.className == "fa-undo fas")) {
+                        that.degree += 90;
+                        if (that.degree == 360) that.degree = 0;
+                        that.scale=1;
                     }
-                    that.changeVal= await that.rotateBase64Image(that.images[0],that.degree);
+                    if ((event.target as any).className == "fa-plus fas" || ((event.target as any).lastChild != null && (event.target as any).lastChild.className == "fa-plus fas")) {
+                        that.scale = that.scale * 1.1; 
+                    }
+                    if ((event.target as any).className == "fa-minus fas" || ((event.target as any).lastChild != null && (event.target as any).lastChild.className == "fa-minus fas")) {
+                        that.scale = that.scale / 1.1; 
+                    }
+                    that.changeVal= await that.rotateBase64Image(that.images[0],that.degree,that.scale);
+                    console.log(that.changeVal);
                 })
-            )
-        }, 500); 
+                )
+        }, 500);
     }
 
     async ngAfterViewInit() {
-        let img = this.value["changingThisBreaksApplicationSecurity"];        
-        let newval= await this.getImageDimensions(img);
-        this.images = [newval.src]; 
-    } 
-    
+        let img = this.value["changingThisBreaksApplicationSecurity"];
+        let newval = await this.getImageDimensions(img);
+        this.images = [newval.src];
+    }
+
     getImageDimensions(file): any {
         return new Promise(function (resolved, rejected) {
             var i = new Image()
@@ -96,8 +104,8 @@ export class ImageModalPage extends AXBasePageComponent {
                     canvas.height = ihScaled;
                     var ctx = canvas.getContext("2d");
                     ctx.drawImage(i, 0, 0, iwScaled, ihScaled);
-                    var dataURL1 = canvas.toDataURL("image/jpeg",0.5);
-                    result= dataURL1.replace(/^data:image\/(png|jpg);base64,/, "");
+                    var dataURL1 = canvas.toDataURL("image/jpeg", 0.5);
+                    result = dataURL1.replace(/^data:image\/(png|jpg);base64,/, "");
                 }
                 resolved({ w: i.width, h: i.height, src: result })
             };
@@ -105,45 +113,85 @@ export class ImageModalPage extends AXBasePageComponent {
         })
     }
 
-    rotateBase64Image(base64data,deg): any {
+    rotateBase64Image(base64data, deg,scl): any {
         return new Promise(function (resolved, rejected) {
             var canvas = document.createElement("canvas");
             var ctx = canvas.getContext("2d");
-         
+
             var i = new Image();
             i.src = base64data;
 
             var cw = i.width, ch = i.height, cx = 0, cy = 0;
             switch (deg) {
                 case 90:
-                  cw = i.height;
-                  ch = i.width;
-                  cy = i.height * (-1);
-                  break;
+                    cw = i.height;
+                    ch = i.width;
+                    cy = i.height * (-1);
+                    break;
                 case 180:
-                  cx = i.width * (-1);
-                  cy = i.height * (-1);
-                  break;
+                    cx = i.width * (-1);
+                    cy = i.height * (-1);
+                    break;
                 case 270:
-                  cw = i.height;
-                  ch = i.width;
-                  cx = i.width * (-1);
-                  break;
-              }
-            canvas.height = ch;
-            canvas.width = cw;
+                    cw = i.height;
+                    ch = i.width;
+                    cx = i.width * (-1);
+                    break;
+            }
  
-            i.onload = function() { 
-                ctx.rotate(deg * Math.PI / 180);  
-                ctx.drawImage(i, cx, cy);  
-                resolved({ w: cw, h: ch, src: canvas.toDataURL() }) 
+            i.onload = function () {
+                let dx = cx;
+                let dy = cy;
+                if (scl != 1) {
+                    if(cx==0)cx=cw;
+                    if(cy==0)cy=ch;
+                    dx = (cx - (cx * scl)) /4;
+                    dy = (cy - (cy * scl)) / 4;  
+                    ch = ch * scl;
+                    cw = cw * scl;
+                    
+                }
+                canvas.height = ch;
+                canvas.width = cw;
+
+                ctx.rotate(deg * Math.PI / 180);
+                if (scl != 1)
+                {
+                    ctx.scale(scl - 0.15, scl - 0.15 ); 
+                    ctx.drawImage(i, dx, dy, cw, ch);
+                }
+                else
+                    ctx.drawImage(i, cx, cy);
+                
+                resolved({ w: cw, h: ch, src: canvas.toDataURL() })
             };
         })
     }
 
 
-    saveChanges()
-    {
-        this.close(this.changeVal?this.changeVal.src:undefined)
-    } 
+    handleFiles(base64data) {
+        var img = new Image();
+        img.src = base64data;
+        var canvas = document.createElement("canvas");
+        var ctx = canvas.getContext("2d");
+        let that = this;
+        img.onload = function () {
+            var iw = img.width;
+            var ih = img.height;
+            var iwScaled = iw * that.scale;
+            var ihScaled = ih * that.scale;
+            debugger
+            var canvas = document.createElement("canvas");
+            canvas.width = iwScaled;
+            canvas.height = ihScaled;
+            canvas.getContext("2d").scale(that.scale - 0.15, that.scale - 0.15);
+            canvas.getContext("2d").drawImage(img, (img.width - canvas.width) / 3, (img.height - canvas.height) / 3, iwScaled, ihScaled);
+
+            console.log(canvas.toDataURL());
+        }
+    }
+
+    saveChanges() {
+        this.close(this.changeVal ? this.changeVal.src : undefined)
+    }
 }
